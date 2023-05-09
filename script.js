0e2fb3ff8f189b096428bd69068980da4628c6dc
Alkallabeth <106618351+Alkallabeth@users.noreply.github.com>
2023-05-09 12:13:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-// Makes it easier to toggle visibility state of <div>s
+// Makes it easier to toggle visibility
 HTMLElement.prototype.hide = function(){
-    this.style.display = "none";
+    this.classList.add("d-none");
 }
 HTMLElement.prototype.show = function(){
-    this.style.display = "block";
+    this.classList.remove("d-none");
 }
 
-// Initializes number of players to 4
 let numberOfPlayers = 4;
 
-// Lets user select number of players
+// Sets number of players based on user selection
 document.getElementById("two-player-btn").addEventListener("click",function(){
     document.getElementById("six-player-btn").classList.remove("active");
     document.getElementById("four-player-btn").classList.remove("active");
     this.classList.add("active");
     numberOfPlayers = 2;
 });
-
 document.getElementById("four-player-btn").addEventListener("click",function(){
     document.getElementById("two-player-btn").classList.remove("active");
     document.getElementById("six-player-btn").classList.remove("active");
     this.classList.add("active");
     numberOfPlayers = 4;
 });
-
 document.getElementById("six-player-btn").addEventListener("click",function(){
     document.getElementById("four-player-btn").classList.remove("active");
     document.getElementById("two-player-btn").classList.remove("active");
@@ -31,7 +28,13 @@ document.getElementById("six-player-btn").addEventListener("click",function(){
     numberOfPlayers = 6;
 });
 
-// Hides start screen when user clicks start button
-document.getElementById("start-game-btn").addEventListener("click",function(){
+// Transitions from start screen to rule screen
+document.getElementById("show-rules-btn").addEventListener("click",function(){
     document.getElementById("start-screen").hide();
+    document.getElementById("rules-screen").show();
+});
+
+//Transitions from rules screen to game screen
+document.getElementById("start-game-btn").addEventListener("click",function(){
+    document.getElementById("rules-screen").hide();
 });
